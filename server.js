0401160867955c1c7dec691db77dff4ac4207b9e
Jeremie Miller <jeremie@jabber.org>
2014-09-29 10:21:16 -0600

    partial progress on the http server conversion, paths only

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
 exports.name = 'http-server';
+var io = require('socket.io');
 
 exports.mesh = function(mesh, cbMesh)
 {
   var args = mesh.args;
-  var io = require("socket.io").listen(args.httpport||0, {log:false});
+  var tp = {pipes:{}};
+  tp.server = io.listen(args.httpport||0, {log:false});
 
-  // guess any local ipv4 just once for http
-  if(!args.http)
-  {
-    var ifaces = require("os").networkInterfaces()
-    var best;
+  // http is primarily for public / non-local usage, so only return the most public path
+  tp.paths = function(){
+    // if there's one manually configured, use that
+    if(mesh.args.http) return [{type:'http',url:mesh.args.http}];
+
+    // just use the best local one
+    var port = io.server.address().port;
+    var ifaces = require('os').networkInterfaces()
+    var local = '127.0.0.1';
+    var best = mesh.public.ipv4; // prefer that if any set
     for (var dev in ifaces) {
       ifaces[dev].forEach(function(details){
-        if(details.family != "IPv4") return;
-        if(!best || !mesh.isLocalIP(details.address) || best == "127.0.0.1") best = details.address;
+        if(details.family != 'IPv4') return;
+        if(details.internal)
+        {
+          local = details.address;
+          return;
+        }
+        if(!best) best = details.address;
       });
-      args.http = "http://"+best+":"+io.server.address().port;
     }
+    best = best||local;
+    return [{type:'http',url:'http://'+best+':'+port}];
+  };
+
+  io.on('connection', function(socket){
     
-  }
-
-  mesh.pathSet({type:"http","http":args.http});    
-
-  io.on("connection", function(socket){
-    socket.on("packet", function(packet) {
+    socket.on('msg', function(packet) {
       if(!packet.data) return;
       mesh.receive((new Buffer(packet.data, "base64")).toString("binary"), {type:"local", id:socket.id});
     });
@@ -35,5 +46,6 @@ exports.mesh = function(mesh, cbMesh)
       io.sockets.sockets[to.id].emit("packet", {data: buf.toString("base64")});
     }
   });
+
 }
 
