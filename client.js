dc131e0285a05bbb57131b42fc9c14cf7fad8815
Jeremie Miller <jeremie@jabber.org>
2014-09-29 16:45:00 -0600

    reorg client to be connection aware, and have a real test

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 exports.name = 'http-client';
-var io = require("socket.io-client");
+var io = require('socket.io-client');
 var urllib = require('url');
+var lob = require('lob-enc');
 
 exports.mesh = function(mesh, cbMesh)
 {
+  var log = mesh.lib.log;
   var tp = {pipes:{}};
 
   // turn a path into a pipe backed by a socket.io client
@@ -15,29 +17,47 @@ exports.mesh = function(mesh, cbMesh)
     var pipe = tp.pipes[id];
     if(pipe) return cbPipe(pipe);
     
-    // create new pipe/socket.io connection
-    pipe = new mesh.lib.Pipe('http-client');
-    tp.pipes[id] = pipe;
-    pipe.id = id;
-    pipe.path = path;
-    pipe.io = io.connect(id);
-    
-    // receive incoming socket.io messages
-    pipe.io.on('msg', function(msg){
-      var packet = lob.decode(new Buffer(msg,'binary'));
-      if(!packet) return mesh.log.info('dropping invalid packet from',pipe.id,msg.toString('hex'));
-      mesh.receive(packet, pipe);
-    });
-    
-    // send packets out to the server as messages
-    pipe.onSend = function(packet, link, cb){
-      // TODO, if channel packet, use .volatile
-      var msg = lob.encode(packet);
-      pipe.io.emit('msg', msg.toString('binary'));
-      cb();
-    }
+    // new socket.io connection attempt
+    var socket = io.connect(id);
 
-    cbPipe(pipe);
+    socket.on('connect', function(){
+      log.debug('connected',id);
+
+      // create new pipe for the connected socket
+      pipe = tp.pipes[id] = new mesh.lib.Pipe('http-client');
+      pipe.id = id;
+      pipe.path = path;
+
+      // receive incoming socket.io messages
+      socket.on('msg', function(msg){
+        var packet = lob.decode(new Buffer(msg,'binary'));
+        if(!packet) return log.info('dropping invalid packet from',pipe.id,msg.toString('hex'));
+        mesh.receive(packet, pipe);
+      });
+    
+      // send packets out to the server as messages
+      pipe.onSend = function(packet, link, cb){
+        if(!socket) return; // disconnected
+        // TODO, if channel packet, use .volatile
+        var msg = lob.encode(packet);
+        socket.emit('msg', msg.toString('binary'));
+        cb();
+      }
+
+      socket.on('disconnect', function(){
+        log.debug('disconnected',pipe.id);
+        if(tp.pipes[pipe.id] == pipe) delete tp.pipes[pipe.id];
+        pipe.emit('keepalive');
+        socket = false;
+      });
+    
+      cbPipe(pipe);
+    });
+
+    // helpful
+    socket.on('error', function(err){
+      log.info('http connection error to',id,err);
+    })
   };
 
   cbMesh(undefined, tp);
